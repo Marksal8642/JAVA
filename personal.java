f259f17256c7f9d8eceed7c9dc2a4358140bfc5f
Francis koroma <98764272+Marksal8642@users.noreply.github.com>
2022-06-12 06:58:10 -0700

    Add files via upload

---
+++ personal.java
@@ -0,0 +1,105 @@
+package praticejava;
+
+import java.util.Random; 
+import java.util.Scanner;
+
+public class Personal {
+           //lottery code  random number/int
+	public static void main(String[] args) {
+	 
+	 
+   /*  int [] array=new int [5];
+       
+       Scanner mk=new Scanner(System.in);
+      Random fk=new Random();
+       		for(int f=3; f<4; f++) {
+       
+      for(int f=0; f<4; f++) {
+      for(int m=0; m<array.length; m++ ) {
+      array[m]=fk.nextInt(50);
+       System.out.println(" "+array[m]); */  
+       
+	
+		/*swapping programm
+   int a= 45, b=74;
+  System.out.println("before swapping :a=:"+a+": b="+b+"");
+         b=a+b;
+         a=b-a;
+         b=b-a;
+    System.out.println("after swapping: a=:"+a+":b="+b+"");*/
+		
+		/*play
+		int month_days[];
+		month_days=new int[12];
+		
+		month_days[0]=31;
+		month_days[1]=28;
+		month_days[2]=31;
+		month_days[3]=30;
+		System.out.println("april has"+month_days[8]);*/
+		
+		/* swappin using thirdvar
+	  
+		int a =45,b=87, temp;
+		System.out.println("berfore swipping\na:="+a+"\nb:="+b);
+		
+		temp=a; 
+		a=b;
+		b=temp;
+		System.out.println("after swipping\na:="+a+"\nb:="+b);*/
+		
+		/*UPPER/LOWERCASE OF STRING
+		String str= "ABCDEF";
+		System.out.println(str.toLowerCase());
+		triming in string
+		String str="love";
+		System.out.println(str.trim());*/
+		
+		/*String name,level,year,gender;
+		Scanner mk=new Scanner(System.in);
+		for(int m=0; m<4; m--) {
+		System.out.println("enter your name");
+		name=mk.next();
+		System.out.println("which food you may like");
+		level=mk.next();
+		System.out.println("when are you comming");
+		year=mk.next();
+		System.out.println("time please");
+		gender=mk.next();
+		System.out.println("your name is:"+name);
+		System.out.println("you oder for:"+level);
+		System.out.println("you will be here at:"+year);
+		System.out.println("date:"+gender);
+		
+      }*/
+    
+         /*   String talk;
+		Scanner mk=new Scanner(System.in);
+		for(int m=3; m<4; m--) {
+		System.out.println("name plase");
+		talk=mk.next();
+		System.out.println("please enter your id");
+		talk=mk.next(); */
+		
+	
+   
+    	
+    	/*Scanner mk=new Scanner(System.in);
+    	System.out.println("even numbers from 20-1");
+    	 for(int m=20; m>1; m--) {
+    		 
+    	 }*/
+		int n=108;
+		
+		System.out.println(Integer.toBinaryString(n));
+		//System.out.println(Integer.toOctalString(n));
+		//System.out.println(Integer.toHexString(n));
+		//System.out.println(Integer.toOctalString(f));
+		//System.out.println(Integer.toOctalString(m));
+    }
+	public static void francis() {
+			Scanner input=new Scanner(System.in);
+		  System.out.println("set passwrd");
+		 }
+	
+}
